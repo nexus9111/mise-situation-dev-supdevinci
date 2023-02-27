340b3618ebfa2f364b34f3cc6e35b14ed3bfc92b
J.Nexus <46869823+nexus9111@users.noreply.github.com>
2023-02-27 11:36:41 +0100

    feat: add login mock

--- front/src/App.js
+++ front/src/App.js
@@ -4,26 +4,33 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import Footer from './components/Footer';
 
 import HomePage from './pages/Home';
-// import MealByCategory from './pages/MealByCategory';
-// import MealCategoryList from './pages/CategoryList';
-// import MealDetail from './pages/MealDetail';
-// import MealList from './pages/MealList';
-// import MealSearch from './pages/MealSearch';
+// import Login from './pages/Login';
+// import Register from './pages/Register';
 
 function App() {
+
+  // const token = getToken();
+
+  // if (!token) {
+  //   return (
+  //     <BrowserRouter>
+  //       <div className="content-layout">
+  //         <Routes> 
+  //           <Route path="/" element={<Login />} />
+  //           <Route path="/register" element={<Register />} />
+  //         </Routes>
+  //       </div>
+  //     </BrowserRouter>
+  //   );
+  // }
+
   return (
     <BrowserRouter>
       <div className="content-layout">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          {/* <Route path="/meal/all" element={<MealList />} />
-          <Route path="/meal/categories" element={<MealCategoryList />} />
-          <Route path="/meal/categories/:category" element={<MealByCategory />} />
-          <Route path="/meal/id/:id" element={<MealDetail />} />
-          <Route path="/meal/name/:name" element={<MealSearch />} /> */}
         </Routes>
       </div>
-      {/* <Footer /> */}
     </BrowserRouter>
   );
 }
