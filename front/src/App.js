b37f78876d33f2a5dcad68d269db59d86806385c
J.Nexus <46869823+nexus9111@users.noreply.github.com>
2023-02-27 10:41:34 +0100

    feat: init project

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// import Footer from './components/Footer';
+
+import HomePage from './pages/Home';
+// import MealByCategory from './pages/MealByCategory';
+// import MealCategoryList from './pages/CategoryList';
+// import MealDetail from './pages/MealDetail';
+// import MealList from './pages/MealList';
+// import MealSearch from './pages/MealSearch';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="content-layout">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          {/* <Route path="/meal/all" element={<MealList />} />
+          <Route path="/meal/categories" element={<MealCategoryList />} />
+          <Route path="/meal/categories/:category" element={<MealByCategory />} />
+          <Route path="/meal/id/:id" element={<MealDetail />} />
+          <Route path="/meal/name/:name" element={<MealSearch />} /> */}
+        </Routes>
+      </div>
+      {/* <Footer /> */}
+    </BrowserRouter>
   );
 }
 
