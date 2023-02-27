a617cb065d635e347609cdc4ff005b1ab85283e2
J.Nexus <46869823+nexus9111@users.noreply.github.com>
2023-02-27 16:22:37 +0100

    refactor: lil restructuration

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,70 +1,84 @@
-import {Button, Checkbox, Form, Input} from 'antd';
-
+import { Button, Checkbox, Form, Input, Col, Row } from "antd";
 
 const onFinish = (value) => {
-  console.log('Success:', value);
-}
+  if (value.retypedPassword !== value.password) {
+    alert("Password does not match");
+    return;
+  }
+  console.log("Success:", value);
+};
 
 const onFinishFailed = (errorInfo) => {
-  console.log('Failed:', errorInfo);
-}
+  console.log("Failed:", errorInfo);
+};
+
 const Home = () => {
   return (
-    <>
-      <h1>Home</h1>
-      <Form
-                name="basic"
-                labelCol={{ span: 8 }}
-                wrapperCol={{ span: 16 }}
-                style={{ maxWidth: 600 }}
-                initialValues={{ remember: true }}
-                onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
-                autoComplete= "off"
-                
-            >
-                <Form.Item
-                    label="Username"
-                    name="username"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'please input your username!'
-                        },
-                    ]}
-                >
-                    <Input />
-                </Form.Item>
-                <Form.Item
-                    label="Password"
-                    name="password"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'please input your password!'
-                        },
-                    ]}
-                    
-                >
-                    <Input.Password />
-                  </Form.Item>
-                  <Form.Item
-                    name="remember"
-                    valuePropName='checked'
-                    wrapperCol={{
-                      offset: 8,
-                      span: 16,
-                    }}
-                  >
-                    <Checkbox> Remember me </Checkbox>
-                  </Form.Item>
-                  <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
-                    <Button type="primary" htmlType="submit">
-                      Submit
-                    </Button>
-                  </Form.Item>
-            </Form>
-    </>
+    <Row justify={"center"}>
+      <Col span={10}>
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "please input your username!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "please input your password!",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            label="Retype Password"
+            name="retypedPassword"
+            rules={[
+              {
+                required: true,
+                message: "please input your password!",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Checkbox> Remember me </Checkbox>
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Col>
+    </Row>
   );
 };
 
