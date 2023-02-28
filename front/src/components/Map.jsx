2872622712dff33d79e8cfd7919e4c03efa64a10
Jonathan SOHIER <sohier.jonathan@gmail.com>
2023-02-28 14:39:56 +0100

    JO

--- front/src/components/Map.jsx
+++ front/src/components/Map.jsx
@@ -5,15 +5,21 @@ import "leaflet/dist/leaflet.css";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 
 const mapComponent = ({ points }) => {
-  console.log(points);
-
+    let center;
+    // console.log(points);
+    if(points.length === 0){
+        console.log(points);
+        center = [48, 2];
+    }else{
+        center = points[0];
+    }
   return (
     <div className="map-container">
       <MapContainer
-        center={[48, 2]}
+        center = {[48, 2]}
         zoom={5}
         scrollWheelZoom={true}
-        style={{ height: "100vh", width: "100%" }}
+        style={{ height: "97vh", width: "100%" }}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -28,6 +34,7 @@ const mapComponent = ({ points }) => {
                   iconUrl: markerIcon,
                   iconSize: [25, 41],
                   iconAnchor: [12, 41],
+                  html: `<span style="background-color : red" />`
                 })
               }
             >
--- front/src/pages/SearchPage.jsx
+++ front/src/pages/SearchPage.jsx
@@ -5,53 +5,54 @@ import Card from "../components/Card.jsx";
 import Map from "../components/Map.jsx";
 
 const SearchPage = () => {
-  const [companies, setCompanies] = useState([]);
-  const [selectedCompany, setSelectedCompany] = useState([]);
+    const [companies, setCompanies] = useState([]);
+    const [selectedCompany, setSelectedCompany] = useState([]);
 
-  useEffect(() => {
-    apiController.searchCompanies("", "", "", "", "", "").then((res) => {
-      setCompanies(res.data.companies);
-    });
-  }, []);
+    useEffect(() => {
+        apiController.searchCompanies("", "", "", "", "", "").then((res) => {
+            setCompanies(res.data.companies);
+        });
+    }, []);
 
-  const selectCompany = (company) => {
-    console.log(company);
-    setSelectedCompany(company);
-  };
+    const selectCompany = (company) => {
+        // console.log(company);
+        setSelectedCompany(company);
+    };
 
-  return (
-    <Row>
-      <Col span={14}>
+    return (
         <Row>
-          {companies.map((company) => {
-            return (
-              // when click on card, select company
-              <Col span={12} onClick={() => selectCompany(company)}>
-                <Card company={company} />
-              </Col>
-            );
-          })}
+            <Col span={14}>
+                <Row>
+                    {companies.map((company) => {
+                        return (
+                            // when click on card, select company
+                            <Col span={12} onClick={() => selectCompany(company)}>
+                                <Card company={company} />
+                            </Col>
+                        );
+                    })}
+                </Row>
+            </Col>
+            <Col span={10}
+                style={{ height: "98vh", width: "100%" }}
+            >
+                {/* if selected company */}
+                {selectedCompany.establishments &&
+                    selectedCompany.establishments.length > 0 ? (
+                    <Map
+                        points={selectedCompany.establishments.map((establishment) => {
+                            return [
+                                parseFloat(establishment.latitude),
+                                parseFloat(establishment.longitude),
+                            ];
+                        })}
+                    />
+                ) : (
+                    <Map points={[]} />
+                )}
+            </Col>
         </Row>
-      </Col>
-      <Col span={10}>
-        <h1>MAP</h1>
-        {/* if selected company */}
-        {selectedCompany.establishments &&
-        selectedCompany.establishments.length > 0 ? (
-          <Map
-            points={selectedCompany.establishments.map((establishment) => {
-              return [
-                parseFloat(establishment.latitude),
-                parseFloat(establishment.longitude),
-              ];
-            })}
-          />
-        ) : (
-          <Map points={[]} />
-        )}
-      </Col>
-    </Row>
-  );
+    );
 };
 
 export default SearchPage;
