03dc8d4034fdb02fcd57033515a4509d09f1c7bd
J.Nexus <46869823+nexus9111@users.noreply.github.com>
2023-02-28 13:59:50 +0100

    feat: update map

--- front/src/components/Map.jsx
+++ front/src/components/Map.jsx
@@ -1,31 +1,43 @@
-import 'leaflet/dist/leaflet.css'
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import { Icon } from "leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+import "leaflet/dist/leaflet.css";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
 
 const mapComponent = ({ points }) => {
+  console.log(points);
 
-    return (
-        <div className='map-container'>
-            <MapContainer center={[48, 2]} zoom={13} scrollWheelZoom={true} style={{
-            }} >
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright%22%3EOpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                {/* {points.map((point) => {
-                return (
-                    <Marker position={point}>
-                        <Popup>
-                            heyy
-                        </Popup>
-                    </Marker>
-                );
-            }
-            )} */}
-
-            </MapContainer>
-        </div>
-
-    );
-}
+  return (
+    <div className="map-container">
+      <MapContainer
+        center={[48, 2]}
+        zoom={5}
+        scrollWheelZoom={true}
+        style={{ height: "100vh", width: "100%" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {points.map((point) => {
+          return (
+            <Marker
+              position={point}
+              icon={
+                new Icon({
+                  iconUrl: markerIcon,
+                  iconSize: [25, 41],
+                  iconAnchor: [12, 41],
+                })
+              }
+            >
+              <Popup>heyy</Popup>
+            </Marker>
+          );
+        })}
+      </MapContainer>
+    </div>
+  );
+};
 
 export default mapComponent;
--- front/src/pages/SearchPage.jsx
+++ front/src/pages/SearchPage.jsx
@@ -1,40 +1,57 @@
 import { Col, Row } from "antd";
-import apiController from "../services/auth"
+import apiController from "../services/auth";
 import { useEffect, useState } from "react";
 import Card from "../components/Card.jsx";
 import Map from "../components/Map.jsx";
 
 const SearchPage = () => {
-    const [companies, setCompanies] = useState([]);
+  const [companies, setCompanies] = useState([]);
+  const [selectedCompany, setSelectedCompany] = useState([]);
 
-    useEffect(() => {
-        apiController.searchCompanies("", "", "", "", "", "").then((res) => {
-            setCompanies(res.data.companies);
-        });
-    }, []);
-    return (
+  useEffect(() => {
+    apiController.searchCompanies("", "", "", "", "", "").then((res) => {
+      setCompanies(res.data.companies);
+    });
+  }, []);
+
+  const selectCompany = (company) => {
+    console.log(company);
+    setSelectedCompany(company);
+  };
+
+  return (
+    <Row>
+      <Col span={14}>
         <Row>
-            <Col span={14}>
-                <Row>
-                    {companies.map((company) => {
-                        return (
-                            <Col span={12}>
-                                <Card company={company}/>
-                            </Col>
-                        );
-                    }
-                    )}
-                </Row>
-            </Col>
-            <Col span={10}>
-                <h1>MAP</h1>
-                <Map points={companies.map((company) => {
-                    company.establishments.map((establishment) => {
-                        return [parseFloat(establishment.latitude), parseFloat(establishment.longitude)];
-                    });
-                })}/>
-            </Col>
-        </Row>);
-}
+          {companies.map((company) => {
+            return (
+              // when click on card, select company
+              <Col span={12} onClick={() => selectCompany(company)}>
+                <Card company={company} />
+              </Col>
+            );
+          })}
+        </Row>
+      </Col>
+      <Col span={10}>
+        <h1>MAP</h1>
+        {/* if selected company */}
+        {selectedCompany.establishments &&
+        selectedCompany.establishments.length > 0 ? (
+          <Map
+            points={selectedCompany.establishments.map((establishment) => {
+              return [
+                parseFloat(establishment.latitude),
+                parseFloat(establishment.longitude),
+              ];
+            })}
+          />
+        ) : (
+          <Map points={[]} />
+        )}
+      </Col>
+    </Row>
+  );
+};
 
 export default SearchPage;
